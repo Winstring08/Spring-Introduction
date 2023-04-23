8197b33429a2689ab7aabf5992490ccf58318d7d
SeungHyeon <r46t8yh547@gmail.com>
2023-04-23 23:53:57 +0900

    API

--- hello-spring/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,9 +1,12 @@
 package hello.hellospring.controller;
 
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.cbor.MappingJackson2CborHttpMessageConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -22,4 +25,39 @@ public class HelloController {
         model.addAttribute( "name", name);
         return "hello-template";
     }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    /*http body부에 데이터를 직접 반환*/
+    /*viewResolver대신 HttpMessageConverter가 동작*/
+    /*기본 문자 처리 : StringHttpMessageConverter
+    기본 객체 처리 : MappingJackson2CborHttpMessageConverter*/
+    public String helloString(@RequestParam("name") String name) {
+        return "hello" + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello; /*객체를 반환하면 JSON으로 반환하는것이 기본세팅*/
+        /*HttpMessageConverter가 동작하여 객체를 JSON스타일로 변경후 요청한 곳에 응답*/
+    }
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+
+
+
+
 }
